7210f66178cca44ff89b138983cd91be979dff0b
andrii.iaremenko <andrii.iaremenko@gmail.com>
2025-03-07 11:52:11 +0200

    changed paths

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
   newAudio.autoplay = true;
 
   let source = document.createElement("source");
-  source.src = "/src/assets/music/Tom%20and%20Jerry%20-%20Is%20You%20Is%20or%20Is%20You%20Aint%20My%20Baby%20-%201946.mp3?nocache=" + new Date().getTime();
+  source.src = "assets/music/song.mp3?nocache=" + new Date().getTime();
   source.type = "audio/mpeg";
 
   newAudio.appendChild(source);
